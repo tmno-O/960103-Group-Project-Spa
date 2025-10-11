71049b21a8e17b38d7e519d4c83d72d2d6d6343d
ddeeww001 <dewthitirat@gmail.com>
2025-10-11 22:59:40 +0700

    not thing

--- bill.java
+++ bill.java
@@ -1,3 +1,6 @@
-public class bill {
-    
+public class bill{
+    public static int printBill(int amount){
+        System.out.println("this is your bill amount: " );
+        return amount;
+    }
 }

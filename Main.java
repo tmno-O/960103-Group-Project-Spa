c8437666e287060bd485c88aa6944db3a1eadb5d
ddeeww001 <dewthitirat@gmail.com>
2025-10-15 18:00:46 +0700

    ....

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-
+//Dew
 public class Main{
     public static void show_Menu(){
         Spa showSpa = new Spa();  // การสร้าง object ขึ้นมาเพื่อดึงข้อมูลมาจากโค้ดของเพื่อน//
         Package showPack = new Package();
         employee showEmp = new employee();
-        System.out.println(showSpa);
+        Bill shoBill= new Bill();
+        /*1System.out.println(showSpa);*/
         System.out.println(showPack);
         System.out.println(showEmp);
+        System.out.println(shoBill);
+    
     }
 }
--- bill.java
+++ bill.java
@@ -1,8 +1,20 @@
+//Dew
 import java.util.Scanner;
-
 public class Bill {
+
+    public void importAll(){
+
+
+    }
+    
+    public void billPackage(){
+
+    }
 
     
 
+    public static void main(String[]agrs){
+
+    }
     
 }

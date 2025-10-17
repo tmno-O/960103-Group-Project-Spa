0ca4257cbed127205e1176925d40e5d7b748f516
tmno-O <kitichet_p@cmu.ac.th>
2025-10-18 03:17:14 +0700

    Update Project in spa method
    improve code

--- Spa.java
+++ Spa.java
@@ -1,54 +1,78 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Spa {
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
+        // Scanner
         Scanner input = new Scanner(System.in);
-        // user id start at 0
-        int user_id = 0;
-        // user info
-        // note 0 is name , 1 is phone , 2 is email
-        String[][] user_info = new String[3][10000];
-        // user usage
-        // 1 is นวดแผนไทย
-        // 2 is นวดน้ำมัน
-        // 3 is นวดประคบสมุนไพร
-        // 4 is ซาวหน้า
-        // 5 is อบสมุนไพร
-        // 6 is อโรม่า
-        // 7 is นวดหลัง
-        // 8 is นวดเท้า
-        // 9  is สปาหน้า
-        String[][] user_usage = new String[9][10000];
-        System.out.println("Hello !! welcome to spa management system.");
+        // Variable
+        int max_user_id = 0;
+        // User Array
+        int[] user_id = new int[10000];
+        String[] user_name = new String[10000];
+        String[] user_phone_number = new String[10000];
+        String[] user_email = new String[10000];
+        // Service List
+        String[] spa_service_list  =  {"นวดแผนไทย","นวดน้ำมัน","นวดประคบสมุนไพร","ซาวหน้า","อบสมุนไพร","อโรม่า","นวดหลัง","นวดเท้า","สปาหน้า"};
+        int[] spa_service_price = {100,100,100,100,100,100,100,100,100};
+        int[] spa_service_income_record = {0,0,0,0,0,0,0,0,0};
+        // User useage
+        int[][] user_usage = new int[9][10000];
+        // Input Variable
+        int input_int;
+        double input_double;
+        String input_String;
+
         while (true) {
-            System.out.println("------------ [ Command Menu ] ------------");
-            System.out.println("Enter 1 to add new user");
-            System.out.println("Enter 2 to edit user usage");
-            System.out.println("Enter 0 to exit");
-            System.out.println("------------------------------------------");
-            System.out.print("Enter your choice here : ");
-            // Variable for select choice
-            int select_choice = input.nextInt();
-            if (select_choice == 1) {
-                System.out.println("------------ [ Add new user ] ------------");
-                System.out.print("Enter user name : ");
-                user_info[0][user_id] = input.next();
-                System.out.print("Enter user phone number : ");
-                user_info[1][user_id] = input.next();
-                System.out.print("Enter user email : ");
-                user_info[2][user_id] = input.next();
-                System.out.println("You added Name : "+user_info[0][user_id]+" , Phone :  "+user_info[1][user_id]+" , Email : "+user_info[2][user_id]+" to user id "+user_id);
-                user_id++;
-            } else if (select_choice == 2) {
-                System.out.println("Enter user id : ");
-                int select_user_id = input.nextInt();
-                if (select_user_id >= user_id) {
-                    System.out.println("User id not found");
-                    continue;
-                } else {
-                    System.out.println("Pap");
-                }
-            } else if (select_choice == 0) {
+            System.out.println("--------------- [ Command Menu ] ---------------");
+            System.out.println("Enter [ 1 ] to add new user");
+            System.out.println("Enter [ 2 ] to edit user usage");
+            System.out.println("Enter [ 3 ] to calculate service charge");
+            System.out.println("Enter [ 4 ] to exit this program");
+            System.out.print("Enter number here : ");
+            input_int = input.nextInt();
+            if (input_int == 5) {
+                System.out.print("Goodbye :)");
                 break;
+            }
+            if (input_int < 0 || input_int > 5) {
+                System.out.print("Error : You input the wrong number");
+                System.out.print("Please try again ..");
+                continue;
+            }
+            if (input_int == 1) {
+                System.out.println("--------------- [ Add New User ] ---------------");
+                System.out.print("User ID : "+max_user_id);
+                user_id[max_user_id] = max_user_id;
+                System.out.print("Enter user name : ");
+                user_name[max_user_id] = input.next();
+                System.out.print("Enter user phone number :");
+                user_phone_number[max_user_id] = input.next();
+                System.out.print("Enter user email : ");
+                user_email[max_user_id] = input.next();
+                System.out.print("");
+                System.out.println("[ ! ] User Information");
+                System.out.println("User id : "+max_user_id);
+                System.out.println("User name : "+user_name[max_user_id]);
+                System.out.println("User phone number : "+user_phone_number[max_user_id]);
+                System.out.println("User email address : "+user_email[max_user_id]);
+                System.out.println("");
+                System.out.println("Confirm this information ?");
+                System.out.println("Enter [ Y ] to confirm is information");
+                System.out.println("Enter [ N ] to cancel");
+                input_String = input.next();
+                input_String = input_String.toUpperCase();
+                if (input_String.equals("N")) {
+                    System.out.println("All user information removed");
+                    user_name[max_user_id] = "";
+                    user_phone_number[max_user_id] = "";
+                    user_email[max_user_id] = "";
+                    continue;
+                }
+                if (input_String.equals("Y")) {
+                    System.out.println("All user information saved at user id "+max_user_id);
+                    max_user_id += 1;
+                    continue;
+                }
             }
         }
     }
